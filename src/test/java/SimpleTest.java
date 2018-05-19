c5a0d2b2de8326e78c8f36cbcf69f245c672389b
panicbrain <kama115@mail.ru>
2018-05-19 16:22:37 +0300

    SimpleTest from Lesson 1

---
+++ src/test/java/SimpleTest.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SimpleTest {
+
+    @Test
+    public void SimpleTest(){
+        System.setProperty("webdriver.chrome.driver", "D:\\Java\\EkaterinaMoldavskaya\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.navigate().to("http://jdi-framework.github.io/tests/index.htm");
+        Assert.assertEquals(driver.getTitle(), "Index Page");
+        WebElement userIcon = driver.findElement(By.cssSelector(".fa-user"));
+        userIcon.click();
+        driver.findElement(By.cssSelector("#Login")).sendKeys("epam");
+        driver.findElement(By.cssSelector("#Password")).sendKeys("1234");
+        driver.findElement(By.cssSelector(".form-horizontal button[type = 'submit']")).click();
+        WebElement userName = driver.findElement(By.cssSelector(".profile-photo span"));
+        Assert.assertTrue(userName.isDisplayed());
+        Assert.assertEquals(userName.getText(),"PITER CHAILOVSKII");
+        driver.close();
+    }
+}
